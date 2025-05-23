fd34f4882fef3665f065eabff56cadd12d57c504
Mostafa Said Abdelrehim <62301014+mossaied2@users.noreply.github.com>
2025-05-23 14:37:04 -0500

    Update Router.cpp

--- Router.cpp
+++ Router.cpp
@@ -1,13 +1,9 @@
-#pragma once
 /*
- * Noxim - the NoC Simulator
- *
- * (C) 2005-2010 by the University of Catania
- * For the complete list of authors refer to file ../doc/AUTHORS.txt
- * For the license applied to these sources refer to file ../doc/LICENSE.txt
- *
- * This file contains the implementation of the router
- */
+authors: Ahmed Khaled & Mostafa Abdelrehim
+Some of the modules are based on Noxim - the NoC Simulator by the University of Catania
+*/
+#pragma once
+
 #include "Router.h"
 
 void Router::reset() { // reset to intial state before any simulation run
