7540ec2f498cca6fe2ba7fe5ab73ea3fbcfb5cd7
Mostafa Said Abdelrehim <62301014+mossaied2@users.noreply.github.com>
2025-05-23 05:42:00 -0500

    Update Buffer.h

--- Buffer.h
+++ Buffer.h
@@ -1,13 +1,9 @@
-#pragma once
 /*
- * Noxim - the NoC Simulator
- *
- * (C) 2005-2010 by the University of Catania
- * For the complete list of authors refer to file ../doc/AUTHORS.txt
- * For the license applied to these sources refer to file ../doc/LICENSE.txt
- *
- * This file contains the declaration of the buffer
+authors: Ahmed Khaled & Mostafa Abdelrehim
+This code is based on Noxim - the NoC Simulator by the University of Catania
  */
+
+#pragma once
 
 #ifndef __BUFFER_H__
 #define __BUFFER_H__
